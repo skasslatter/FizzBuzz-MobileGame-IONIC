f07808b7d3e8dd9ba78739ac11734c28f636b747
sybille <sybille@pxlwidgets-heroes.com>
2020-08-31 16:23:47 +0200

    initial setup of Camera Plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {IonicStorageModule} from "@ionic/storage";
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 @NgModule({
     declarations: [AppComponent],
@@ -22,7 +24,8 @@ import {IonicStorageModule} from "@ionic/storage";
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        Camera,
     ],
     exports: [],
     bootstrap: [AppComponent]
--- src/app/pages/highscore/highscore.page.ts
+++ src/app/pages/highscore/highscore.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 
 @Component({
-  selector: 'app-highscore',
-  templateUrl: './highscore.page.html',
-  styleUrls: ['./highscore.page.scss'],
+    selector: 'app-highscore',
+    templateUrl: './highscore.page.html',
+    styleUrls: ['./highscore.page.scss'],
 })
 export class HighscorePage implements OnInit {
+    clickedImage: string;
+    options: CameraOptions = {
+        quality: 30,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+    }
 
-  constructor() { }
+    constructor(
+        private camera: Camera,
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    captureImage() {
+        this.camera.getPicture(this.options).then((imageData) => {
+            this.clickedImage = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            console.log(err);
+        });
+    }
 }
