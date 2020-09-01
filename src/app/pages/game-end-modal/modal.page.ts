a1001c613cdacf54647362194b39949c55cf4faf
sybille <sybille@pxlwidgets-heroes.com>
2020-09-01 10:06:38 +0200

    basic Camera Plugin is working

--- src/app/pages/game-end-modal/modal.page.ts
+++ src/app/pages/game-end-modal/modal.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {Storage} from '@ionic/storage';
+import {Camera, CameraOptions} from "@ionic-native/camera/ngx";
 
 @Component({
     selector: 'app-modal',
@@ -9,33 +10,65 @@ import {Storage} from '@ionic/storage';
 })
 
 export class ModalPage implements OnInit {
+    clickedPhoto: string;
+    highscoreHolderPhoto: string;
+    options: CameraOptions = {
+        quality: 30,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+    }
     score: number;
     highscore: number = null;
     isNewHighScore: boolean = false;
 
     constructor(
         private modalCtrl: ModalController,
-        private storage: Storage
+        private storage: Storage,
+        private camera: Camera,
     ) {
     }
 
     ngOnInit() {
         // this.storage.clear();
+        this.storage.get('photo')
+            .then((photo) => {
+                this.highscoreHolderPhoto = photo
+            }, (err) => {
+                console.log(err);
+            })
 
         this.storage.get('highscore')
             .then(value => {
                 if (value === null || this.score > value) {
                     this.storage.set('highscore', this.score)
-                    this.highscore = this.score
-                    this.isNewHighScore = true
+                        .then((r) => {
+                            this.highscore = this.score
+                            this.isNewHighScore = true
+                        })
                 } else {
                     this.highscore = value
                     this.isNewHighScore = false
                 }
-            })
+            }, (err) => {
+                console.log(err);
+            });
     }
 
-    dismiss() {
+    dismissModal() {
         this.modalCtrl.dismiss();
+    }
+
+    captureImage() {
+        this.camera.getPicture(this.options)
+            .then((imageData) => {
+                this.clickedPhoto = 'data:image/jpeg;base64,' + imageData;
+                this.storage.set('photo', this.clickedPhoto)
+                    .then((r) => {
+                        this.highscoreHolderPhoto = this.clickedPhoto
+                    })
+            }, (err) => {
+                console.log(err);
+            });
     }
 }
