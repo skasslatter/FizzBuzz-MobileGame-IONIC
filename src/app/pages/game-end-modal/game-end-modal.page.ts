08c34835ae3a6e56dc298894f96829d6ed21906c
sybille <sybille@pxlwidgets-heroes.com>
2020-09-03 15:55:04 +0200

    changed default Camera direction to front

--- src/app/pages/game-end-modal/game-end-modal.page.ts
+++ src/app/pages/game-end-modal/game-end-modal.page.ts
@@ -15,7 +15,8 @@ export class GameEndModalPage implements OnInit {
         quality: 30,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
+        mediaType: this.camera.MediaType.PICTURE,
+        cameraDirection: 1,
     };
     userPhoto: string;
     userName: string;
